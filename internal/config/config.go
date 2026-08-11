07e4e7a1478a5d7562839e6bbf517bd33f33df54
nahuel <nahuel.dev.23@gmail.com>
2026-08-11 10:44:53 -0300

    retention session env

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,25 +1,41 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
 	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	DatabaseURL string
-	Port        string
-	JWTSecret   string
+	DatabaseURL          string
+	Port                 string
+	JWTSecret            string
+	SessionRetentionDays int
 }
 
 func Load() (*Config, error) {
+
 	err := godotenv.Load()
 	if err != nil {
 		return nil, err
 	}
 
+	retention := os.Getenv("SESSION_RETENTION_DAYS")
+
+	if retention == "" {
+		retention = "90"
+	}
+
+	sessionRetentionDays, err := strconv.Atoi(retention)
+	if err != nil {
+		return nil, err
+	}
+
 	return &Config{
-		DatabaseURL: os.Getenv("DATABASE_URL"),
-		Port:        os.Getenv("PORT"),
-		JWTSecret:   os.Getenv("JWT_SECRET"),
+		DatabaseURL:          os.Getenv("DATABASE_URL"),
+		Port:                 os.Getenv("PORT"),
+		JWTSecret:            os.Getenv("JWT_SECRET"),
+		SessionRetentionDays: sessionRetentionDays,
 	}, nil
 }
